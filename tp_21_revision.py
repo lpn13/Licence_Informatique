702d65572a6d576d4bc073855e880a286741fe90
Léon-Paul NEU <89944008+lpn13@users.noreply.github.com>
2022-05-02 19:19:09 +0200

    Update tp_21_revision.py

--- tp_21_revision.py
+++ tp_21_revision.py
@@ -1,16 +1,61 @@
-def pair_impair(liste):
+import random
+import collections
 
-    pair = list(filter(lambda x: x % 2 == 0, liste))
-    impair = list(filter(lambda x: x % 2 == 1, liste))
+#====================================================#
 
-    print( "Nombres pairs :", pair)
-    print("Nombres impair :", impair)
+def rand_list(n):
+    return random.sample(range(-10, 10), n)
 
-liste_int = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print("Liste d'entiers :", liste_int)
-pair_impair(liste_int)
+# print(rand_list(10))
 
-#=================#
+#====================================================#
+
+def pair_impair(list1, list2):
+
+    pair1 = list(filter(lambda x: x % 2 == 0, list1))
+    pair2 = list(filter(lambda x: x % 2 == 0, list2))
+    
+    impair1 = list(filter(lambda x: x % 2 == 1, list1))
+    impair2 = list(filter(lambda x: x % 2 == 1, list2))
+
+    list_pair = sorted(pair1 + pair2)
+    list_impair = sorted(impair1 + impair2)
+
+    print( "Nombres pairs :", list_pair)
+    print("Nombres impair :", list_impair)
+
+# liste_int1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+# liste_int2 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+# print("Liste d'entiers :", liste_int1 + liste_int2)
+# pair_impair(liste_int1, liste_int2)
+
+#====================================================#
+
+def maxi(liste):
+
+    maxi = liste[0]
+    
+    for index in range(1, len(liste)):
+        if maxi < liste[index]:
+            maxi = liste[index]
+        index += 1
+
+    return maxi
+
+# Liste_int = [-5, 6, 12, -34, 9, 987]
+# print("Liste :", Liste_int)
+# print("Max :", maxi(Liste_int))
+
+#====================================================#
+
+def concatain_list(l1, l2):
+    return sorted(l1+l2)
+
+# liste1, liste2 = [1, 3, 5, 9, 2, 6], [4, 7, 8, 10]
+# print(concatain_list(liste1, liste2))
+
+#====================================================#
 
 def palindromes(liste):
 
@@ -18,13 +63,11 @@ def palindromes(liste):
 
     print("Palindrome(s) :", ", ".join(map(str, result)))
 
-liste_str = ["geeks", "geeg", "keegs", "practice", "aa"]
-print("Liste de mots :", liste_str)
-palindromes(liste_str)
+# liste_str = ["geeks", "geeg", "keegs", "practice", "aa"]
+# print("Liste de mots :", liste_str)
+# palindromes(liste_str)
 
-#=================#
-
-import collections
+#====================================================#
 
 def anagrammes(liste):
 
@@ -33,6 +76,6 @@ def anagrammes(liste):
 
     print("Anagmrme(s) de {} :".format(mot), ", ".join(map(str, result)))
 
-mot = "eegks"
-liste_str = ["geeks", "geeg", "keegs", "practice", "aa"]
-anagrammes(liste_str)
+# mot = "eegks"
+# liste_str = ["geeks", "geeg", "keegs", "practice", "aa"]
+# anagrammes(liste_str)
